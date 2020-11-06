8d8d85b5fc70f7f0ef5f30d023a6969ee58b9b73
adammgourley <adammgourley@gmail.com>
2020-11-06 11:05:21 -0500

    Updated 'youtube.py' format. It can be imported now for 'main.py'

--- youtube.py
+++ youtube.py
@@ -1,55 +1,53 @@
-import os, json
-import pickle
-import pprint
+import os, json, pickle, pprint
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from googleapiclient.discovery import build
 
-credentials = None
+class Youtube:
+    def __init__(self):
+        self.API_KEY = os.getenv('API_KEY') if os.getenv('API_KEY') is not None else input('Enter Youtube API Key: ')
+        self.credentials = None
+        self.set_credentials()
+        self.youtube = build("youtube", "v3", developerKey=self.API_KEY)
+        self.url = input("Enter Youtube Playlist URL: ").split("?list=")
+        self.all_videos = []
+        self.get_playlist(self.url)
 
-if os.path.exists("token.pickle"):
-    print("Loading Credentials from File...")
-    with open("token.pickle", "rb") as token:
-        credentials = pickle.load(token)
+    def set_credentials(self):
+        if os.path.exists('token.pickle'):
+            print('Loading Credentials from File')
+            with open('token.pickle', 'rb') as token:
+                self.credentials = pickle.load(token)
 
-# If there are no valid credentials available, then either refresh the token or log in.
-if not credentials or not credentials.valid:
-    if credentials and credentials.expired and credentials.refresh_token:
-        print('Refreshing Access Token...')
-        credentials.refresh(Request())
-    else:
-        print('Fetching New Tokens...')
-        flow = InstalledAppFlow.from_client_secrets_file(
-            'client_secrets.json',
-            scopes=[
-                'https://www.googleapis.com/auth/youtube.readonly'],
-            )
+        if not self.credentials or not self.credentials.valid:
+            if self.credentials and self.credentials.expired and self.credentials.refresh_token:
+                print('Refreshing Access Token...')
+                self.credentials.refresh(Request())
+            else:
+                print('Fetching New Tokens...')
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'client_secrets.json',
+                    scopes=['https://www.googleapis.com/auth/youtube.readonly'],
+                )
+                flow.run_local_server(port=8080, prompt='consent', authorization_prompt_message='')
+                self.credentials = flow.credentials
 
-        flow.run_local_server(port=8080, prompt='consent',
-                              authorization_prompt_message='')
-        credentials = flow.credentials
+                # Save for the next run
+                with open('token.pickle', 'wb') as f:
+                    print('Saving Credentials for Future Use...')
+                    pickle.dump(self.credentials, f)
+    
+    def get_playlist(self, url, next_page=None):
+        request = self.youtube.playlistItems().list(part=['snippet'], playlistId=url[-1], pageToken=next_page)
+        response = request.execute()
 
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as f:
-            print('Saving Credentials for Future Use...')
-            pickle.dump(credentials, f)
+        for video in response['items']:
+            self.all_videos.append(video['snippet']['title'])
+        
+        if "nextPageToken" in response:
+            self.get_playlist(url, response['nextPageToken'])
+    
+    
 
-
-API_KEY = os.getenv("API_KEY")
-youtube = build("youtube", "v3", developerKey=API_KEY)
-playlist_url = input("Playlist URL: ").split("?list=")
-
-all_videos = []
-def get_playlist(url, next_page=None):
-    request = youtube.playlistItems().list(part=["snippet"], playlistId=playlist_url[-1], pageToken=next_page)
-    response = request.execute()
-
-    for video in response['items']:
-        all_videos.append(video['snippet']['title'])
-
-    if "nextPageToken" in response:
-        get_playlist(url, response['nextPageToken'])
-
-get_playlist(playlist_url)
-
-print(all_videos)
+yt = Youtube()
+print(yt.all_videos)
